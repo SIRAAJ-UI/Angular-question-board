e032d050b1c368e1a8d92d826c8d0d42c6b47d49
Sirajudeen <ksiraj2002@gmail.com>
2022-07-20 16:02:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { NodeService } from './nodeservice';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rnd';
+  chapter: any[];
+  marksList: any[] = [];
+  marksListItem: any[] = [];
+  usersForm: FormGroup;
+  mainTotal = 0;
+
+  constructor(private nodeService: NodeService) { }
+
+
+  ngOnInit() {
+    this.nodeService.getFilesystem().then(files => this.chapter = files);
+  }
+
+ 
+
+  trackByMethod(index: number, el: any): number {
+    return el.key;
+  }
+  onMarkChange(chapter,concept, markIndex, $event) {
+    const targetedValue = $event.target.value;
+    concept.marksHolder[markIndex] = Number(markIndex) * Number(targetedValue);
+    const keys = Object.keys(concept.marksHolder);
+    let total = 0;
+    keys.forEach((key, index) => {
+      total += Number(concept.marksHolder[key]);
+    });
+    concept.total = total;
+    this.checkSubTotal(chapter);
+    this.checkOverAllMarks();
+  }
+
+  private checkSubTotal(chapter) {
+    let subTotal = 0;
+    chapter.concepts.forEach( _concept => {
+        subTotal += _concept.total
+      })
+    chapter.totalMarks = subTotal;
+  };
+
+  private checkOverAllMarks() {
+      let overallTotal = 0;
+      this.chapter.forEach( _chapter => {
+        _chapter.concepts.forEach( _concept => {
+          overallTotal += _concept.total
+        })
+      })
+    this.mainTotal = overallTotal;
+  }
+
+  onChangebox(chapter,concept,$event) {
+    if(concept.isSelected === false) {
+       chapter.totalMarks = 0;
+       let filterByChapter = this.chapter.filter( __chapter => { return ( __chapter.chapterId === chapter.chapterId)})
+        filterByChapter[0].concepts.forEach( __concept => {
+          __concept.marksHolder = {
+            "1":0,
+            "2":0,
+            "3":0,
+            "5":0,
+            "10":0
+        }
+        for(let i=0;i<5;i++) {
+          const id = `a${concept.conceptId}-${i}`;
+          (<HTMLInputElement>document.getElementById(id)).value = "0"
+        }
+        if(concept.conceptId === __concept.conceptId) {
+          concept.total = 0;
+          this.checkSubTotal(chapter)
+          this.checkOverAllMarks();
+        }
+        })
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AccordionModule } from 'primeng/accordion';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { CheckboxModule } from 'primeng/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NodeService } from './nodeservice'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AccordionModule,
+    HttpClientModule,
+    FormsModule, ReactiveFormsModule,
+    BrowserAnimationsModule,
+    CheckboxModule,
+    DropdownModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [NodeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
